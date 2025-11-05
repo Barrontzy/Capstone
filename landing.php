736475272327dd01d6bc6dcc8e8c5a100a6228c1
barron <barrondelapena@gmail.com>
2025-11-05 22:30:05 +0800

    test

--- landing.php
+++ landing.php
@@ -290,7 +290,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 </div>
 
                 <div class="recaptcha-container">
-                    <div class="g-recaptcha" data-sitekey="6LeGTvwrAAAAAHP4_YhvsnBqH6iRyliAh0Ed_U8E"></div>
+                    <div class="g-recaptcha" data-sitekey="6LfHZwMsAAAAAJTnl2A5sKGDmVTFmBKjEy3bh9q0"></div>
                 </div>
 
                 <button type="submit" class="btn-login">
